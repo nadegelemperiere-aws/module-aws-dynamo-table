f8a1161a1bf8badf7fe861134ed93ed56772e2e4
Nadege LEMPERIERE <nadege.lemperiere@gmail.com>
2022-03-14 14:30:58 +0100

    feat/cicd : First version

---
+++ main.tf
@@ -0,0 +1,110 @@
+# -------------------------------------------------------
+# TECHNOGIX
+# -------------------------------------------------------
+# Copyright (c) [2022] Technogix SARL
+# All rights reserved
+# -------------------------------------------------------
+# Module to deploy an aws dynamodb with all the secure
+# components required
+# -------------------------------------------------------
+# Nadège LEMPERIERE, @21 january 2021
+# Latest revision: 21 january 2021
+# -------------------------------------------------------
+
+# -------------------------------------------------------
+# Create the dynamodb
+# -------------------------------------------------------
+resource "aws_dynamodb_table" "table" {
+
+    name = "${var.project}-${var.environment}-${var.region}-${var.name}"
+
+    read_capacity      = var.capacity.read
+    write_capacity     = var.capacity.write
+
+    billing_mode       = "PROVISIONED"
+
+    hash_key           = var.hash
+    range_key          = var.range
+
+    server_side_encryption {
+        enabled     = true
+        kms_key_arn = aws_kms_key.table.arn
+    }
+
+    point_in_time_recovery {
+        enabled     = true
+    }
+
+    dynamic "attribute" {
+        for_each = var.attributes
+        content {
+            name = attribute.value.name
+            type = attribute.value.type
+        }
+    }
+
+    tags = {
+        Name            = "${var.project}.${var.environment}.${var.module}.${var.region}.${var.name}.table"
+        Environment     = var.environment
+        Owner           = var.email
+        Project         = var.project
+        Version         = var.git_version
+        Module          = var.module
+    }
+}
+
+
+# -------------------------------------------------------
+# Set permission policy for table access
+# -------------------------------------------------------
+locals {
+    kms_statements = concat([
+        for i,right in (("${var.rights}" != null) ? "${var.rights}" : []) :
+        {
+            Sid           = right.description
+            Effect        = "Allow"
+            Principal     = {
+                "AWS"         : (("${right.principal.aws}" != null) ? "${right.principal.aws}" : [])
+                "Service"     : (("${right.principal.services}" != null) ? "${right.principal.services}" : [])
+            }
+            Action         = ["kms:Decrypt","kms:GenerateDataKey"],
+            Resource    = ["*"]
+        }
+    ],
+    [
+        {
+            Sid         = "AllowRootAndServicePrincipal"
+            Effect         = "Allow"
+            Principal     = {
+                "AWS"         : ["arn:aws:iam::${var.account}:root", "arn:aws:iam::${var.account}:user/${var.service_principal}"]
+            }
+            Action         = "kms:*",
+            Resource    = ["*"]
+        }
+    ])
+}
+
+# -------------------------------------------------------
+# Table encryption key
+# -------------------------------------------------------
+resource "aws_kms_key" "table" {
+
+    description                 = "Table ${var.name} encryption key"
+    key_usage                    = "ENCRYPT_DECRYPT"
+    customer_master_key_spec    = "SYMMETRIC_DEFAULT"
+    deletion_window_in_days        = 7
+    enable_key_rotation            = true
+    policy                        = jsonencode({
+          Version = "2012-10-17",
+          Statement = "${local.kms_statements}"
+    })
+
+    tags = {
+        Name            = "${var.project}.${var.environment}.${var.module}.${var.region}.${var.name}.table.key"
+        Environment     = var.environment
+        Owner           = var.email
+        Project         = var.project
+        Version         = var.git_version
+        Module          = var.module
+    }
+}
---
+++ test/keywords/data.py
@@ -0,0 +1,58 @@
+# -------------------------------------------------------
+# TECHNOGIX
+# -------------------------------------------------------
+# Copyright (c) [2022] Technogix SARL
+# All rights reserved
+# -------------------------------------------------------
+# Keywords to create data for module test
+# -------------------------------------------------------
+# Nadège LEMPERIERE, @13 november 2021
+# Latest revision: 13 november 2021
+# -------------------------------------------------------
+
+# System includes
+from json import load, dumps
+
+# Robotframework includes
+from robot.libraries.BuiltIn import BuiltIn, _Misc
+from robot.api import logger as logger
+from robot.api.deco import keyword
+ROBOT = False
+
+# ip address manipulation
+from ipaddress import IPv4Network
+
+@keyword('Load Multiple Test Data')
+def load_multiple_test_data(ids, arns, key, region) :
+
+    result = {}
+    result['tables'] = []
+
+    if len(ids) != 2 : raise Exception(str(len(ids)) + ' tables created instead of 2')
+
+    for i in range(1,2) :
+        table = {}
+        table['TableName'] = 'test-test-' + region + '-test-' + str(i)
+        table['AttributeDefinitions'] = [{'AttributeName' : 'LockID', 'AttributeType' : 'S'}]
+        table['ItemCount'] = 0
+        table['KeySchema'] = [{'AttributeName' : 'LockID', 'KeyType' : 'HASH'}]
+        table['TableStatus'] = 'ACTIVE'
+        table['TableArn'] = arns[i - 1]
+        table['SSEDescription'] = {'Status' : 'ENABLED', 'SSEType' : 'KMS', 'KMSMasterKeyArn': key[i - 1] }
+
+        if i == 1 : table['ProvisionedThroughput'] = {'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5}
+        elif i == 2 : table['ProvisionedThroughput'] = {'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1}
+
+        table['Tags'] = []
+        table['Tags'].append({'Key'        : 'Version'             , 'Value' : 'test'})
+        table['Tags'].append({'Key'        : 'Project'             , 'Value' : 'test'})
+        table['Tags'].append({'Key'        : 'Module'              , 'Value' : 'test'})
+        table['Tags'].append({'Key'        : 'Environment'         , 'Value' : 'test'})
+        table['Tags'].append({'Key'        : 'Owner'               , 'Value' : 'moi.moi@moi.fr'})
+        table['Tags'].append({'Key'        : 'Name'                , 'Value' : 'test.test.test.' + region + '.test-' + str(i) + '.table'})
+
+        result['tables'].append({'name' : 'test-' + str(i), 'data' : table})
+
+    logger.debug(dumps(result))
+
+    return result
